cc65e6e0672b1fcb99e15f50e525931c0b31b831
rdv1k <43339320+rdv1k@users.noreply.github.com>
2022-08-03 17:33:53 +0200

    Add Playlist component

---
+++ musicmaster/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TrackList from '../Tracklist/TrackList';
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+    render() {
+      return (
+        <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList />
+        </div>
+      );
+    }
+  }
+  
+  export default SearchResults;
+  
