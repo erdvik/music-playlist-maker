1baba2f7105c62b7482e56a379ab0ef06208c2d3
rdv1k <43339320+rdv1k@users.noreply.github.com>
2022-08-06 11:44:23 +0200

    Add change name of a playlist

--- musicmaster/src/Components/App/App.js
+++ musicmaster/src/Components/App/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -32,6 +33,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: playlistTracks});
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -45,7 +50,8 @@ class App extends React.Component {
             <Playlist 
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack} />
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
